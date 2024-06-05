adecd8f3268bd271b50b9c238013ee5cbc3b8f36
calhounpaul <paul.c.calhoun@gmail.com>
2024-06-05 11:00:50 +0000

    initial commit

---
+++ build_docker_api_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#rm -rf vllm
+
+if [ ! -d "vllm" ]; then
+    git clone https://github.com/vllm-project/vllm
+    #git checkout 919770957f26d71a5a6eda7a1a7443dfeb5ba0ee
+    cd vllm
+    sed -i 's/ENTRYPOINT \[/#ENTRYPOINT \[/g' Dockerfile
+    echo "ENTRYPOINT [\"python3\", \"-m\", \"outlines.serve.serve\"]" >> Dockerfile
+    cd ..
+fi
+cd vllm
+docker build -t outlines_vllm_server .
---
+++ run_docker_api_server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+HF_TOKEN=""
+if [ -f secrets.json ]; then
+    HF_TOKEN=$(jq -r '.HF_TOKEN' secrets.json)
+fi
+
+HF_CACHE_FOLDER_PATH=./cache
+
+docker run --rm -it \
+    -v $HF_CACHE_FOLDER_PATH:/root/.cache/huggingface \
+    --gpus='"device=0"' \
+    -p 8000:8000 \
+    --env "HUGGING_FACE_HUB_TOKEN=$HF_TOKEN" \
+    --ipc=host \
+    -m 90g \
+    outlines_vllm_server \
+    --guided-decoding-backend outlines \
+    --dtype auto \
+    --max-model-len 32000 \
+    --model mistralai/Mistral-7B-Instruct-v0.3
+#    --model neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit
+#    --model mistralai/Mistral-7B-Instruct-v0.3
+#    --model solidrust/Mistral-7B-Instruct-v0.3-AWQ
+#    --model NousResearch/Hermes-2-Theta-Llama-3-8B
+#    --model nvidia/Llama3-ChatQA-1.5-8B
+#    --model casperhansen/llama-3-8b-instruct-awqd
+#    --model TheBloke/Mistral-7B-Instruct-v0.2-AWQ
+#    --model casperhansen/mixtral-instruct-awq
+#    --model TheBloke/OpenHermes-2.5-Mistral-7B-16k-AWQ
